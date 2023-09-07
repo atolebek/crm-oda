7c61d1b9b0a82d475137db5570d657d0bd3b528a
alitolebek <ali.tolebek@salesintel.io>
2023-09-07 11:10:58 +0600

    electricity

--- src/main/java/kz/tele2/crmoda/controller/admin/AdminController.java
+++ src/main/java/kz/tele2/crmoda/controller/admin/AdminController.java
@@ -2,7 +2,9 @@ package kz.tele2.crmoda.controller.admin;
 
 import io.swagger.annotations.Api;
 import kz.tele2.crmoda.dto.response.rent.ClientRentsResponse;
+import kz.tele2.crmoda.model.Electricity;
 import kz.tele2.crmoda.model.Rent;
+import kz.tele2.crmoda.repository.ElectricityRepository;
 import kz.tele2.crmoda.repository.RentRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.annotation.Secured;
@@ -15,23 +17,35 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/admin/rents")
+@RequestMapping("/admin")
 @Api(tags = "Rents")
 @RequiredArgsConstructor
 public class AdminController {
 
     private final RentRepository rentRepository;
+    private final ElectricityRepository electricityRepository;
 
-    @GetMapping
+    @GetMapping("/rents")
     public List<Rent> getRents(){
         String username = SecurityContextHolder.getContext().getAuthentication().getName();
         return rentRepository.findAll();
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("/rents/{id}")
     public Rent getRent(@PathVariable Long id){
         String username = SecurityContextHolder.getContext().getAuthentication().getName();
         return rentRepository.getById(id);
     }
 
+    @GetMapping("/electricities")
+    public List<Electricity> getElectricities(){
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        return electricityRepository.findAll();
+    }
+
+    @GetMapping("/electricities/{id}")
+    public Electricity getElectricity(@PathVariable Long id){
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        return electricityRepository.getById(id);
+    }
 }
